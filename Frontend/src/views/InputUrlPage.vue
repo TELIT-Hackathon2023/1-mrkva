a7669f2c2540268a28331067cc33663b85e4b686
danielamitnikova <dadamitnikova@gmail.com>
2023-11-25 17:56:22 +0100

    header

--- Frontend/src/views/InputUrlPage.vue
+++ Frontend/src/views/InputUrlPage.vue
@@ -1,17 +1,31 @@
 <template>
-    <div id="app">
-      <!-- Top Bar -->
-      <div class="top-bar">
-        <img src="path_to_telecom_logo.png" alt="Telecom Logo" class="logo"/>
-        <!-- Add more elements to your top bar here if needed -->
-      </div>
+  <v-card color="white" flat height="200px" rounded="0">
+    <v-toolbar color="pink" density="compact">
+      <v-app-bar-nav-icon>
+        <img src="@/assets/tsystems2.png" alt="T-Systems Logo" style="height: 30px; width: 30px;"/>
+      </v-app-bar-nav-icon>
+
+      <v-toolbar-title>UX Analyzer</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
   
-      <!-- Main Content -->
-      <div class="main-content">
-        <input type="text" class="search-input" placeholder="Search...">
-      </div>
-    </div>
-  </template>
+  <img src="@/assets/logoIQ.png" alt="T-Systems Logo" style="height: 30px; width: 30px;"/>
+  
+</template>
   
   <script>
   export default {
